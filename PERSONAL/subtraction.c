8d0d3b308078f7cf6155036ab117367cb6af2e69
XP2002 <xavpass@gmail.com>
2024-07-09 23:17:01 +0000

    created subtraction code

---
+++ PERSONAL/subtraction.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+    int a, b, c, result
+
+    printf("enter 3 integers");
+    scanf("%d %d %d", &a, &b, &c);
+
+    result = a - b - c
+
+    printf("%d - %d - %d\n", a, b, c, result);
+    return 0;
+}
